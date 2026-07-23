4e91fe7e773cf97fd6c695d7f7c24ea51221cddd
Bhavya Sengar <bhavyahts@gmail.com>
2026-07-23 16:12:32 +0530

    Task 2 Update

--- Lab1/EventLoop/Task2.js
+++ Lab1/EventLoop/Task2.js
@@ -1,14 +1,32 @@
+import fs from "fs/promises";
+
+const writeData = async () => {
+  try {
+    console.log("about to write...");
+    await fs.writeFile("stud.txt", "Name : Raman Singh");
+    console.log("File Written");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const f1 = () => {
   console.log("f1");
 };
 const f2 = () => {
   console.log("f2");
 };
+const f3 = () => {
+  console.log("f3");
+};
 
 const main = () => {
   console.log("main");
-  setTimeout(f1, 5000);
-  f2();
+  setTimeout(f1, 0);
+  //setInterval(f2, 1000);
+  setImmediate(f2);
+  process.nextTick(f3);
+  writeData();
   console.log("end");
 };
 
