affefab904d53ecd1418e4b951f02524ab639492
Bhavya Sengar <bhavyahts@gmail.com>
2026-07-28 09:45:25 +0530

    read file

--- Lab2/index.js
+++ Lab2/index.js
@@ -1,4 +1,7 @@
-import { writeFile } from "fs/promises";
+import { writeFile, readFile } from "fs/promises";
 
-await writeFile("Student.text", "Ravikant Singh\nRoll No. 82");
-console.log("File Written");
+// await writeFile("Student.txt", "Ravikant Singh\nRoll No. 82");
+// console.log("File Written");
+
+const data = await readFile("student.txt", "utf-8");
+console.log(`file content: ${data}`);
